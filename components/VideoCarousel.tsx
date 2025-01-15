aeb66b61196c0d79b65442677a89190014ba68b0
stat42 <Shock.Sw.001@gmail.com>
2025-01-14 19:25:17 -0500

    change tab label text

--- components/VideoCarousel.tsx
+++ components/VideoCarousel.tsx
@@ -162,7 +162,7 @@ const VideoCarousel = ({ primaryVideos = [], relatedVideos = [] }: VideoCarousel
                   : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
               }`}
             >
-              Related Videos
+              Equipment-Related Videos
             </button>
           </div>
         </div>
